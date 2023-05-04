92bcb554843d2db6f39cab23418ac1762b5085fa
Fwedpat <97058670+Fwedpat@users.noreply.github.com>
2023-05-04 14:21:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+import Experience from "./Experience/Experience.js";
+
+const experience = new Experience(document.querySelector(".experience-canvas"));
+document.getElementById('myGithub').addEventListener('click', function (event) {
+window.location.href = "http://www.w3schools.com";
+  }, false);
